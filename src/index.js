e3d05a92826770d0386a47757cf0b6a547c93e69
Baptiste <baptiste.greve@gmail.com>
2019-10-29 16:38:34 +0100

    Published beta version

--- src/index.js
+++ src/index.js
@@ -23,6 +23,27 @@ export const Colors = {
   grey: "#6a668e"
 };
 
+/*export const Colors = {
+  textPrimary: "#000000",
+  textSecondary: "#435363",
+  textTertiary: "#435363",
+
+  mainBackground: "#f7f8fc",
+  secondaryBackground: "#ffffff",
+  darkBackground01: "#11112b",
+  darkBackground02: "#191735",
+
+  positive: "#7ffacc",
+  negative: "#e85e5e",
+
+  green: "#46d8a3",
+  blue: "#29cae4",
+  yellow: "#fdb640",
+  pink: "#e74b86",
+  white: "#ffffff",
+  grey: "#6a668e"
+};*/
+
 /** Main Component */
 export const Main = props => {
   return (
